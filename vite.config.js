272c9b5d13c181e46938e275c92af6db1c3f169f
Nora_Chung <k82811@gmail.com>
2026-01-08 14:52:49 +0800

    初版

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => {
   return {
     plugins: [react()],
-    base: command === 'build' ? '/react-task-week2/' : '/',
+    base: command === 'build' ? '/react-task-week3/' : '/',
     css: {
       preprocessorOptions: {
         scss: {
